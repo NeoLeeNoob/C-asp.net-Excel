622bde467d29de24f37812c9ac3adca10c1a6ba2
NeoLeeNoob <lee0989371853@gmail.com>
2024-06-05 15:47:58 +0800

    Add files via upload

---
+++ ECS.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ECS.aspx.cs" Inherits="ECS.ECS" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="Button1" runat="server" Text="上傳" OnClick="Button1_Click" />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ECS.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+
+namespace ECS
+{
+    public partial class ECS : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string fileName = FileUpload1.FileName;
+                string filePath = Server.MapPath("~/App_Data") + "\\" + fileName;
+
+                // 讀取特定工作表的資料
+                string sheetName = "工作表1";
+
+                using (OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=Excel 12.0 XML;"))
+                {
+                    conn.Open();
+
+                    string sql = "SELECT * FROM [" + sheetName + "$]";
+                    using (OleDbCommand cmd = new OleDbCommand(sql, conn))
+                    {
+                        using (OleDbDataReader reader = cmd.ExecuteReader())
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Load(reader);
+
+                            // 自訂 GridView 控制項的欄位
+                            GridView1.AutoGenerateColumns = false;
+
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                BoundField field = new BoundField();
+                                field.DataField = reader.GetName(i);
+                                field.HeaderText = reader.GetName(i);
+
+                                GridView1.Columns.Add(field);
+                            }
+
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
