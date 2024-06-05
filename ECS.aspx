a3e7f6efe188e509957c6e07718a11672726b2de
NeoLeeNoob <lee0989371853@gmail.com>
2024-06-05 15:50:18 +0800

    Add files via upload

--- ECS.aspx
+++ ECS.aspx
@@ -1,18 +1,50 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ECS.aspx.cs" Inherits="ECS.ECS" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ECS.aspx.cs" Inherits="ECS.ECS2" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
     <title></title>
-</head>
-<body>
+    </head>
+    <body>
     <form id="form1" runat="server">
         <div>
-        <asp:FileUpload ID="FileUpload1" runat="server" />
-        <asp:Button ID="Button1" runat="server" Text="上傳" OnClick="Button1_Click" />
-        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+            <br/>
+            <asp:Button ID="Button1" runat="server" Text="新增" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="修改" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="查詢" OnClick="Button3_Click" />
+            <br/>
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            <br/>
+            <asp:CheckBox ID="CheckBox1" runat="server" Text="名字" OnCheckedChanged="CheckBox1_CheckedChanged1" Checked="true" AutoPostBack="True" />
+            <asp:CheckBox ID="CheckBox2" runat="server" Text="公司" OnCheckedChanged="CheckBox2_CheckedChanged2" Checked="true" AutoPostBack="True" />
+            <asp:CheckBox ID="CheckBox3" runat="server" Text="料號" OnCheckedChanged="CheckBox3_CheckedChanged3" Checked="true" AutoPostBack="True" />
+            <asp:CheckBox ID="CheckBox4" runat="server" Text="地址" OnCheckedChanged="CheckBox4_CheckedChanged4" Checked="true" AutoPostBack="True" />
+            <asp:CheckBox ID="CheckBox5" runat="server" Text="料件" OnCheckedChanged="CheckBox5_CheckedChanged5" Checked="true" AutoPostBack="True" />
+            <br/>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:TemplateField>
+            <ItemTemplate>
+                <asp:ImageButton ID="ImageButton1" runat="server" ImageUrl="https://localhost:44345/images/REDATA.JPG" OnClick="ImageButton1_Click" Height="30px" Width="30px" />
+                <asp:ImageButton ID="ImageButton2" runat="server" ImageUrl="https://localhost:44345/images/DELETE.JPG" OnClick="ImageButton2_Click" Height="30px" Width="30px" />
+            </ItemTemplate>
+        </asp:TemplateField>
+         <asp:BoundField DataField="ID" HeaderText="ID" SortExpression="ID"   Visible="True" />
+        <asp:BoundField DataField="名字" HeaderText="名字" SortExpression="名字" Visible="True"/>
+        <asp:BoundField DataField="公司" HeaderText="公司" SortExpression="公司" Visible="True"/>
+        <asp:BoundField DataField="料號" HeaderText="料號" SortExpression="料號" Visible="True"/>
+        <asp:BoundField DataField="地址" HeaderText="地址" SortExpression="地址" Visible="True"/>
+        <asp:BoundField DataField="料件" HeaderText="料件" SortExpression="料件" Visible="True"/>
+    </Columns>
+</asp:GridView>
         </div>
     </form>
 </body>
--- ECS.aspx.cs
+++ ECS.aspx.cs
@@ -1,63 +1,331 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.OleDb;
-using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.OleDb;
 using System.Data;
+using System.Data.OleDb;
+using System.Data.Common;
+using System.Diagnostics.Eventing.Reader;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+using System.Windows.Input;
+using static OfficeOpenXml.ExcelErrorValue;
 
 namespace ECS
 {
-    public partial class ECS : System.Web.UI.Page
+    public partial class ECS2 : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        string connetionString = "provider=Microsoft.ACE.OLEDB.12.0; Data Source=D:\\ECS\\ECS\\ECS\\ECS\\App_Data\\個人資料1.xlsx ; Extended Properties = \"Excel 12.0 Xml;HDR=yes; IMEX = 1\" ";
 
-        }
-        protected void Button1_Click(object sender, EventArgs e)
+        protected void CheckBox1_CheckedChanged1(object sender, EventArgs e)//勾選顯示隱藏......................................................................................................................................
         {
-            if (FileUpload1.HasFile)
+            GridView1.Columns[2].Visible = CheckBox1.Checked;
+            if (CheckBox1.Checked == false)
             {
-                string fileName = FileUpload1.FileName;
-                string filePath = Server.MapPath("~/App_Data") + "\\" + fileName;
-
-                // 讀取特定工作表的資料
-                string sheetName = "工作表1";
-
-                using (OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=Excel 12.0 XML;"))
-                {
-                    conn.Open();
-
-                    string sql = "SELECT * FROM [" + sheetName + "$]";
-                    using (OleDbCommand cmd = new OleDbCommand(sql, conn))
-                    {
-                        using (OleDbDataReader reader = cmd.ExecuteReader())
-                        {
-                            DataTable dt = new DataTable();
-                            dt.Load(reader);
-
-                            // 自訂 GridView 控制項的欄位
-                            GridView1.AutoGenerateColumns = false;
-
-                            for (int i = 0; i < reader.FieldCount; i++)
-                            {
-                                BoundField field = new BoundField();
-                                field.DataField = reader.GetName(i);
-                                field.HeaderText = reader.GetName(i);
-
-                                GridView1.Columns.Add(field);
-                            }
-
-                            GridView1.DataSource = dt;
-                            GridView1.DataBind();
-                        }
-                    }
-                }
+                for (int i = 1; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[2].Visible = false; }
+            }
+            else
+            {
+                for (int i = 1; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[2].Visible = true; }
             }
         }
+        protected void CheckBox2_CheckedChanged2(object sender, EventArgs e)//勾選顯示隱藏......................................................................................................................................
+        {
+            GridView1.Columns[3].Visible = CheckBox2.Checked;
+            if (CheckBox2.Checked == false)
+            {
+                for (int i = 2; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[3].Visible = false; }
+            }
+            else
+            {
+                for (int i = 2; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[3].Visible = true; }
+            }
+        }
+        protected void CheckBox3_CheckedChanged3(object sender, EventArgs e)//勾選顯示隱藏......................................................................................................................................
+        {
+            GridView1.Columns[4].Visible = CheckBox3.Checked;
+            if (CheckBox3.Checked == false)
+            {
+                for (int i = 3; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[4].Visible = false; }
+            }
+            else
+            {
+                for (int i = 3; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[4].Visible = true; }
+            }
+        }
+        protected void CheckBox4_CheckedChanged4(object sender, EventArgs e)//勾選顯示隱藏......................................................................................................................................
+        {
+            GridView1.Columns[5].Visible = CheckBox4.Checked;
+            if (CheckBox4.Checked == false)
+            {
+                for (int i = 4; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[5].Visible = false; }
+            }
+            else
+            {
+                for (int i = 4; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[5].Visible = true; }
+            }
+        }
+        protected void CheckBox5_CheckedChanged5(object sender, EventArgs e)//勾選顯示隱藏......................................................................................................................................
+        {
+            GridView1.Columns[6].Visible = CheckBox5.Checked;
+            if (CheckBox5.Checked == false)
+            {
+                for (int i = 5; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[6].Visible = false; }
+            }
+            else
+            {
+                for (int i = 5; i < GridView1.Rows.Count; i++)
+                { GridView1.Rows[i].Cells[6].Visible = true; }
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)//網站頁面........................................................................................................................................................................................
+        {
+            string sql = "SELECT TOP 10 * FROM  [工作表1$] ORDER BY ID DESC ";
+            if (!IsPostBack)
+            {
+                using (OleDbConnection connection = new OleDbConnection(connetionString))
+                {
+                    using (OleDbDataAdapter dataadapter = new OleDbDataAdapter(sql, connection))
+                    {
+                        connection.Open();
+                        DataSet ds = new DataSet();
+                        dataadapter.Fill(ds);
+                        GridView1.DataSource = ds.Tables[0];
+                        GridView1.DataBind();
+                        connection.Close();
+                    }
+                }
+                GridView1.HeaderRow.Cells[1].Visible = false; for (int i = 0; i < GridView1.Rows.Count; i++) { GridView1.Rows[i].Cells[1].Visible = false; }
+            };
+        }
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)  //獲取該列資料得已修改按鈕..........................................................................................................
+        {
+            // 獲取 ImageButton 所在的資料列
+            GridViewRow row = (GridViewRow)((ImageButton)sender).NamingContainer;
+            // 獲取要修改的資料列的資料
+            string swpa = row.Cells[1].Text;
+            string value1 = row.Cells[2].Text;
+            string value2 = row.Cells[3].Text;
+            string value3 = row.Cells[4].Text;
+            string value4 = row.Cells[5].Text;
+            string value5 = row.Cells[6].Text;
+            TextBox1.Text = value1;
+            TextBox2.Text = value2;
+            TextBox3.Text = value3;
+            TextBox4.Text = value4;
+            TextBox5.Text = value5;
+            ViewState["swpa"] = swpa;
+        }
+        protected void ImageButton2_Click(object sender, ImageClickEventArgs e)  //刪除按鈕.............................................................................................................................................
+        {
+            // 獲取 ImageButton 所在的資料列
+            GridViewRow row = (GridViewRow)((ImageButton)sender).NamingContainer;
+            // 獲取要修改的資料列的資料
+            string DELID = row.Cells[1].Text;
+            string sql = "UPDATE [工作表1$] SET ID = Null, 名字 = Null, 公司 = Null, 料號 = Null, 地址 = Null, 料件 = Null WHERE ID = " + DELID + "";
+            using (OleDbConnection connection = new OleDbConnection(connetionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            // 重新讀取資料並更新 GridView1
+            sql = "SELECT TOP 10 * FROM  [工作表1$] WHERE 名字 IS NOT NULL AND 公司 IS NOT NULL ORDER BY ID DESC  ";
+            using (OleDbConnection connection = new OleDbConnection(connetionString))
+            {
+                using (OleDbDataAdapter dataadapter = new OleDbDataAdapter(sql, connection))
+                {
+                    connection.Open();
+                    DataSet ds = new DataSet();
+                    dataadapter.Fill(ds);
+                    GridView1.DataSource = ds.Tables[0];
+                    GridView1.DataBind();
+                    connection.Close();
+                }
+            }
+            GridView1.HeaderRow.Cells[1].Visible = false;
+            for (int i = 0; i < GridView1.Rows.Count; i++) { GridView1.Rows[i].Cells[1].Visible = false; }
+        }
+        protected void Button1_Click(object sender, EventArgs e)  //新增按鈕................................................................................................................................................................................
+        {
+            if (TextBox1.Text == "")
+            {
+                // 如果 TextBox1.Text 值為 null，顯示錯誤訊息
+                Label1.Text = "請先填寫完整要新增的資料。";
+                return;
+            }
+            else if (TextBox2.Text == "")
+            {
+                // 如果 TextBox2.Text 值為 null，顯示錯誤訊息
+                Label1.Text = "請先填寫完整要新增的資料。";
+                return;
+            }
+            String value1 = TextBox1.Text;
+            String value2 = TextBox2.Text;
+            String value3 = TextBox3.Text;
+            String value4 = TextBox4.Text;
+            String value5 = TextBox5.Text;
+            //先抓取最大的ID
+            int ID = 0;
+            String sql = "SELECT MAX(ID) FROM [工作表1$] ";
 
+            using (OleDbConnection connection = new OleDbConnection(connetionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(sql, connection))
+                {
+                    connection.Open();
+                    OleDbDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        ID = Convert.ToInt32(reader[0]);
+                    }
+                    connection.Close();
+                }
+            }
+            //再新增一筆資料
+            ID = ID + 1;
+            sql = "INSERT INTO [工作表1$] (ID, 名字, 公司, 料號, 地址, 料件) VALUES ('" + ID + "','" + value1 + "','" + value2 + "','" + value3 + "','" + value4 + "','" + value5 + "')";
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            Label1.Text += ID + sql;
+
+            using (OleDbConnection connection = new OleDbConnection(connetionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+          /*  Response.Redirect(Request.Url.AbsoluteUri);
+            GridView1.HeaderRow.Cells[1].Visible = false;
+            for (int i = 0; i < GridView1.Rows.Count; i++) { GridView1.Rows[i].Cells[1].Visible = false; }*/
+        }
+        protected void Button2_Click(object sender, EventArgs e)  //修改按鈕................................................................................................................................................................................
+        {
+            // 檢查 ViewState 中的 swpa 值是否為 null
+            if (ViewState["swpa"] == null)
+            {
+                // 如果 swpa 值為 null，顯示錯誤訊息
+                Label1.Text = "請先選擇要修改的資料列。";
+                return;
+            }
+            // 獲取要修改的資料列的資料
+            string swpa = (string)ViewState["swpa"];
+            string upvalue1 = TextBox1.Text;
+            string upvalue2 = TextBox2.Text;
+            string upvalue3 = TextBox3.Text;
+            string upvalue4 = TextBox4.Text;
+            string upvalue5 = TextBox5.Text;
+
+            // 更新資料庫中的資料
+            string sql = "UPDATE [工作表1$] SET 名字 = @upvalue1, 公司 = @upvalue2, 料號 = @upvalue3, 地址 = @upvalue4, 料件 = @upvalue5 WHERE ID = @swpa";
+            using (OleDbConnection connection = new OleDbConnection(connetionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@upvalue1", upvalue1);
+                    command.Parameters.AddWithValue("@upvalue2", upvalue2);
+                    command.Parameters.AddWithValue("@upvalue3", upvalue3);
+                    command.Parameters.AddWithValue("@upvalue4", upvalue4);
+                    command.Parameters.AddWithValue("@upvalue5", upvalue5);
+                    command.Parameters.AddWithValue("@swpa", swpa);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            // 將 ViewState 中的 swpa 值設為 null
+            ViewState["swpa"] = null;
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            // 重新讀取資料並更新 GridView1
+            sql = "SELECT TOP 10 * FROM  [工作表1$] WHERE 名字 IS NOT NULL AND 公司 IS NOT NULL ORDER BY ID DESC";
+            using (OleDbConnection connection = new OleDbConnection(connetionString))
+            {
+                using (OleDbDataAdapter dataadapter = new OleDbDataAdapter(sql, connection))
+                {
+                    connection.Open();
+                    DataSet ds = new DataSet();
+                    dataadapter.Fill(ds);
+                    GridView1.DataSource = ds.Tables[0];
+                    GridView1.DataBind();
+                    connection.Close();
+                }
+            }
+            GridView1.HeaderRow.Cells[1].Visible = false;
+            for (int i = 0; i < GridView1.Rows.Count; i++)
+            {
+                GridView1.Rows[i].Cells[1].Visible = false;
+            }
+            Response.Redirect(Request.Url.AbsoluteUri);
+        }
+        protected void Button3_Click(object sender, EventArgs e)  //查詢按鈕.................................................................................................................................................................................
+        {
+            String value1 = TextBox1.Text;
+            String value2 = TextBox2.Text;
+            String value3 = TextBox3.Text;
+            String value4 = TextBox4.Text;
+            String value5 = TextBox5.Text;
+            String sql = "SELECT TOP 10 * FROM  [工作表1$] WHERE 名字 IS NOT NULL AND 公司 IS NOT NULL ORDER BY ID DESC";
+            if (!String.IsNullOrEmpty(value1) || !String.IsNullOrEmpty(value2) || !String.IsNullOrEmpty(value3) || !String.IsNullOrEmpty(value4) || !String.IsNullOrEmpty(value5))
+            {
+                sql = "SELECT * FROM [工作表1$] WHERE 1 = 1";
+                if (!String.IsNullOrEmpty(value1)) { sql += "AND (名字 IS NULL OR 名字 LIKE @value1)"; }
+                if (!String.IsNullOrEmpty(value2)) { sql += "AND (公司 IS NULL OR 公司 LIKE @value2)"; }
+                if (!String.IsNullOrEmpty(value3)) { sql += "AND (料號 IS NULL OR 料號 LIKE @value3)"; }
+                if (!String.IsNullOrEmpty(value4)) { sql += "AND (地址 IS NULL OR 地址 LIKE @value4)"; }
+                if (!String.IsNullOrEmpty(value5)) { sql += "AND (料件 IS NULL OR 料件 LIKE @value5)"; }
+                sql += "AND NOT (名字 IS NULL AND 公司 IS NULL AND 料號 IS NULL AND 地址 IS NULL AND 料件 IS NULL)";
+
+            }
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            using (OleDbConnection connection = new OleDbConnection(connetionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(sql, connection))
+                {
+                    if (!String.IsNullOrEmpty(value1)) { command.Parameters.AddWithValue("@value1", "%" + value1 + "%"); }
+                    if (!String.IsNullOrEmpty(value2)) { command.Parameters.AddWithValue("@value2", "%" + value2 + "%"); }
+                    if (!String.IsNullOrEmpty(value3)) { command.Parameters.AddWithValue("@value3", "%" + value3 + "%"); }
+                    if (!String.IsNullOrEmpty(value4)) { command.Parameters.AddWithValue("@value4", "%" + value4 + "%"); }
+                    if (!String.IsNullOrEmpty(value5)) { command.Parameters.AddWithValue("@value5", "%" + value5 + "%"); }
+                    sql += "AND NOT (名字 IS NULL AND 公司 IS NULL AND 料號 IS NULL AND 地址 IS NULL AND 料件 IS NULL)";
+                    connection.Open();
+                    DataSet ds = new DataSet();
+                    OleDbDataAdapter dataadapter = new OleDbDataAdapter(command);
+                    dataadapter.Fill(ds);
+                    GridView1.DataSource = ds.Tables[0];
+                    GridView1.DataBind();
+                    connection.Close();
+                }
+            }
+            GridView1.HeaderRow.Cells[1].Visible = false;
+            for (int i = 0; i < GridView1.Rows.Count; i++) { GridView1.Rows[i].Cells[1].Visible = false; }
+        }
     }
 }
